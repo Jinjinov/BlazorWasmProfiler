454c7f17d25cdc3c9216c84589e8bce14c27004d
Urban <dzindzinovic@hotmail.com>
2023-08-04 00:35:03 +0200

    Version 0.0.1.1

--- BlazorWasmProfiler/ExecutionData.cs
+++ BlazorWasmProfiler/ExecutionData.cs
@@ -29,10 +29,16 @@ namespace BlazorWasmProfiler
             {
                 _isTiming = false;
 
-                TotalTime += Stopwatch.GetElapsedTime(_startTime);
+                TotalTime += GetElapsedTime(_startTime);
 
                 Count++;
             }
         }
+
+        private const long TicksPerMillisecond = 10000;
+        private const long TicksPerSecond = TicksPerMillisecond * 1000;
+        private static readonly double s_tickFrequency = (double)TicksPerSecond / Stopwatch.Frequency;
+        private static TimeSpan GetElapsedTime(long startingTimestamp) => GetElapsedTime(startingTimestamp, Stopwatch.GetTimestamp());
+        private static TimeSpan GetElapsedTime(long startingTimestamp, long endingTimestamp) => new TimeSpan((long)((endingTimestamp - startingTimestamp) * s_tickFrequency));
     }
 }
